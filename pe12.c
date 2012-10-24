93cf11cdee4d046ac1b0001f3c47582a892f6b59
Lu Shengliang <lushl9301@hotmail.com>
2012-10-24 13:26:50 +0800

    花了很久……无语了……

---
+++ pe12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int
+main()
+{
+    int count = 1;
+    int i = 70;
+    while (count<500)
+    {
+	count = 1;
+	int div = 2;
+	int now = (i+1)*i/2;
+        while (now > 1)
+	{
+	    int t = 0;
+	    while (now % div == 0)
+	    {
+		now /= div;
+		t++;
+	    }
+	    count *= t+1;
+	    div++;
+	}
+	i++;
+    }
+    printf("%d", (i-1)*i/2);
+}
